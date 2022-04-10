8c92f1cacff80c33deaf791ee2bb04f595f2b679
kumarohan0115 <rohankr350@gmail.com>
2022-04-10 18:51:32 +0530

    Committed

---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+    const [show, handleShow]=useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 100){
+                handleShow(true);
+            }
+            else
+            handleShow(false);
+        })
+        return ()=>{
+            window.removeEventListener("scroll");
+        }
+    },[]);
+    
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+        <img className='nav_logo' src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="Netflix_logo" />
+
+        <img className='nav_avtar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avrat logo" />
+    </div>
+  )
+}
+
+export default Navbar
